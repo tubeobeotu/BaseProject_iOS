144fae03f5bc3382434aeb9dc511cde77c2419f4
Tea <tea@anpsoft.com>
2018-04-20 10:32:16 +0700

    update

--- MVC_Swift/BaseProject/BaseProject/Utils/Extensions/Array+Extension.swift
+++ MVC_Swift/BaseProject/BaseProject/Utils/Extensions/Array+Extension.swift
@@ -4,12 +4,26 @@
 
 import Foundation
 
-extension Array {
+
+extension Array where Element: Equatable {
+    
+    @discardableResult mutating func remove(object: Element) -> Bool {
+        if let index = index(of: object) {
+            self.remove(at: index)
+            return true
+        }
+        return false
+    }
+    
+    @discardableResult mutating func remove(where predicate: (Array.Iterator.Element) -> Bool) -> Bool {
+        if let index = self.index(where: { (element) -> Bool in
+            return predicate(element)
+        }) {
+            self.remove(at: index)
+            return true
+        }
+        return false
+    }
     
 }
 
-extension Array where Element: Equatable  {
-	mutating func delete(element: Iterator.Element) {
-		self = self.filter{$0 != element }
-	}
-}
