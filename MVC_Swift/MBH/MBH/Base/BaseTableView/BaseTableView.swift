23aaa41571b5c92392944f0934d1b4c5eb0c44bb
tunv <>
2018-08-08 14:57:26 +0700

    update

---
+++ MVC_Swift/MBH/MBH/Base/BaseTableView/BaseTableView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseTableView.swift
+//  MBH
+//
+//  Created by tunv on 8/8/18.
+//  Copyright © 2018 tunv. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
